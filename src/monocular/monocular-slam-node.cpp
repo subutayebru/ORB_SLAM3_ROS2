5ac601036df2f8cd31eea1962d899bd65126c301
E.Subutay <subutayebru@gmail.com>
2024-11-15 16:23:05 +0100

    read compressed image

--- src/monocular/monocular-slam-node.cpp
+++ src/monocular/monocular-slam-node.cpp
@@ -1,43 +1,55 @@
 #include "monocular-slam-node.hpp"
-
-#include<opencv2/core/core.hpp>
-
-using std::placeholders::_1;
+#include <opencv2/imgcodecs.hpp>
 
 MonocularSlamNode::MonocularSlamNode(ORB_SLAM3::System* pSLAM)
-:   Node("ORB_SLAM3_ROS2")
+    : Node("ORB_SLAM3_ROS2")
 {
     m_SLAM = pSLAM;
-    // std::cout << "slam changed" << std::endl;
-    m_image_subscriber = this->create_subscription<ImageMsg>(
-        "camera",
-        10,
+
+    // Create QoS profile to match the bag file
+    auto qos = rclcpp::QoS(rclcpp::KeepLast(3))
+        .best_effort()
+        .durability_volatile();
+
+    m_image_subscriber = this->create_subscription<sensor_msgs::msg::CompressedImage>(
+        "/uuv02/vertical_camera/image_raw/compressed",
+        qos,
         std::bind(&MonocularSlamNode::GrabImage, this, std::placeholders::_1));
-    std::cout << "slam changed" << std::endl;
+
+    RCLCPP_INFO(this->get_logger(), "SLAM node initialized");
 }
 
 MonocularSlamNode::~MonocularSlamNode()
 {
-    // Stop all threads
     m_SLAM->Shutdown();
-
-    // Save camera trajectory
     m_SLAM->SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
 }
 
-void MonocularSlamNode::GrabImage(const ImageMsg::SharedPtr msg)
+void MonocularSlamNode::GrabImage(const sensor_msgs::msg::CompressedImage::SharedPtr msg)
 {
-    // Copy the ros image message to cv::Mat.
     try
     {
-        m_cvImPtr = cv_bridge::toCvCopy(msg);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
-        return;
-    }
+        // Decode the compressed image
+        cv::Mat image = cv::imdecode(cv::Mat(msg->data), cv::IMREAD_UNCHANGED);
+        
+        if (image.empty())
+        {
+            RCLCPP_ERROR(this->get_logger(), "Failed to decode compressed image");
+            return;
+        }
 
-    std::cout<<"one frame has been sent"<<std::endl;
-    m_SLAM->TrackMonocular(m_cvImPtr->image, Utility::StampToSec(msg->header.stamp));
+        // Get the timestamp in seconds
+        double timestamp = msg->header.stamp.sec + msg->header.stamp.nanosec * 1e-9;
+
+        RCLCPP_INFO(this->get_logger(), "Processing frame at timestamp: %f", timestamp);
+
+        // Process the frame with ORB-SLAM3
+        m_SLAM->TrackMonocular(image, timestamp);
+
+        RCLCPP_INFO(this->get_logger(), "Frame processed successfully");
+    }
+    catch (const std::exception& e)
+    {
+        RCLCPP_ERROR(this->get_logger(), "Exception in GrabImage: %s", e.what());
+    }
 }
--- src/monocular/monocular-slam-node.hpp
+++ src/monocular/monocular-slam-node.hpp
@@ -2,9 +2,10 @@
 #define __MONOCULAR_SLAM_NODE_HPP__
 
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/image.hpp"
+#include "rclcpp/qos.hpp"
+#include "sensor_msgs/msg/compressed_image.hpp"
 
-#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
 
 #include "System.h"
 #include "Frame.h"
@@ -17,19 +18,14 @@ class MonocularSlamNode : public rclcpp::Node
 {
 public:
     MonocularSlamNode(ORB_SLAM3::System* pSLAM);
-
     ~MonocularSlamNode();
 
 private:
-    using ImageMsg = sensor_msgs::msg::Image;
-
-    void GrabImage(const sensor_msgs::msg::Image::SharedPtr msg);
+    void GrabImage(const sensor_msgs::msg::CompressedImage::SharedPtr msg);
 
     ORB_SLAM3::System* m_SLAM;
 
-    cv_bridge::CvImagePtr m_cvImPtr;
-
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr m_image_subscriber;
+    rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr m_image_subscriber;
 };
 
 #endif
